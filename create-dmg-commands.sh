95754a168ad43807973d3b0cd51ebe8872eeb90e
Neil Sims <neil.david.sims@gmail.com>
2023-09-29 23:03:09 +0100

    DMG codesigned

--- create-dmg-commands.sh
+++ create-dmg-commands.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
+rm TeXHelp.dmg
 create-dmg \
    --app-drop-link 530 218 \
    --icon TeXHelp.app 130 218 \
    --eula TeXHelp/COPYING \
+   --codesign - \
    --background Background.png \
    --window-size 660 400 \
    TeXHelp.dmg TeXHelp.app
