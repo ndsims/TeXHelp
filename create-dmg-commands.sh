81f38fd0fe35411919167f148342b58c229d85b3
Neil Sims <neil.david.sims@gmail.com>
2023-09-30 14:54:40 +0100

    Codesigned

--- create-dmg-commands.sh
+++ create-dmg-commands.sh
@@ -4,7 +4,7 @@ create-dmg \
    --app-drop-link 530 218 \
    --icon TeXHelp.app 130 218 \
    --eula TeXHelp/COPYING \
-   --codesign - \
+   --codesign "Developer ID Application: Neil Sims (22BF9TB59Q)" \
    --background Background.png \
    --window-size 660 400 \
    TeXHelp.dmg TeXHelp.app
